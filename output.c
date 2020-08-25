1555d3554cdc200770d2b91f6ebfce12b6adde08
Wind2375like <wind2375like@163.com>
2020-08-25 12:01:53 +0800

    修改output.c

--- output.c
+++ output.c
@@ -1,14 +1,28 @@
-int k(int v);
 int buf(int a);
+int k(int v);
 int main(int argc, char *argv[]){
 	int a;
+	int arr[MAXLENGTH];
+	int b;
+	int brr[MAXLENGTH];
+	a++;
+	b=k(a);
+	a=buf(b);
+	swap(a, b)
 	int i;
 	for(i=1;i<=10;i++){
-		a=a+1;
+		a=a+2*i;
 	}
 	while(a>=0){
-		a=a-1;
+		a=a-2*i;
 	}
+	if(a==0&&a!=b){
+		a+=b;
+	}
+	for(i=a;i<=b;i++){
+		brr[i++]=i;
+	}
+	for(int i = 0; i <= b-a; i++) arr[a+i]=brr[b+i];
 	return 0;
 }
 int buf(int a){
